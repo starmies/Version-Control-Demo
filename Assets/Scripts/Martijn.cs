20b41475904d030a6aaf7b096de61272fe8046f5
JB <J_C_333@hotmail.com>
2021-08-09 15:12:40 +0200

    te late commit

---
+++ Assets/Scripts/Martijn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Martijn : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name=="A")
+        {
+            Instantiate(gameObject);
+        }
+        else if (col.gameObject.tag == "cube")
+        {
+            Instantiate(gameObject);
+        }
+    }
+}
